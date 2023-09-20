eb9d472f47efb013cc17795b84b9e847b2872801
Lucas Josino <contact@lucasjosino.com>
2023-09-19 21:48:06 -0300

    build: Remove scripts and Bump retype version

--- scripts/js/update-colors.mjs
+++ scripts/js/update-colors.mjs
@@ -5,6 +5,8 @@ import * as fs from "fs";
 main();
 
 async function main() {
+  console.log("[update-colors] Updating colors...");
+
   const json = fs.readFileSync("./_assets/colors.json", {
     encoding: "utf8",
     flag: "r",
@@ -12,13 +14,15 @@ async function main() {
   const colors = new Map(Object.entries(JSON.parse(json)));
 
   fs.readFile("./build/resources/css/retype.css", "utf-8", (err, data) => {
-    if (err) return console.log(err);
+    if (err) return console.error(err);
 
     let result = data;
     colors.forEach((value, key) => (result = result.replaceAll(key, value)));
 
     fs.writeFile("./build/resources/css/retype.css", result, "utf8", (err) => {
-      if (err) return console.log(err);
+      if (err) return console.error(err);
     });
   });
+
+  console.log("[update-colors] Updated colors!");
 }
